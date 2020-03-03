ad85725d4f928016a396851301fdb6c033fdf5c4
QualIT-AKL <32342625+QualIT-AKL@users.noreply.github.com>
2020-03-03 15:22:47 +1300

    Update TwitterSmokeStepDefinition.java

--- java-maven-test/src/test/java/stepdefinitions/TwitterSmokeStepDefinition.java
+++ java-maven-test/src/test/java/stepdefinitions/TwitterSmokeStepDefinition.java
@@ -1,61 +1,57 @@
 package stepdefinitions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import pageobjs.HomePage;
 import pageobjs.LoginPage;
 import pageobjs.TwitterUserHomePage;
 import utilities.BrowserDriver;
 
 public class TwitterSmokeStepDefinition {
-	
-	private Scenario scenario;
-	HomePage hPage;
-	LoginPage loginP;
-	TwitterUserHomePage twitterHome;
 
-	@Before
-	public void setupBrowser(Scenario scenario) {
-		this.scenario = scenario;
-		final String webAppUrl = System.getProperty("URL");
-		// System.out.println("\"" + System.getProperty("timestamp") + "\"");
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+    private Scenario scenario;
+    HomePage hPage;
+    LoginPage loginP;
+    TwitterUserHomePage twitterHome;
 
-		// set the driver based on property above
-		BrowserDriver.setDriver(new ChromeDriver());
-		BrowserDriver.getDriver().get(webAppUrl);
-	}
+    @Before
+    public void setupBrowser(Scenario scenario) {
+        this.scenario = scenario;
+        final String webAppUrl = System.getProperty("URL");
+        // System.out.println("\"" + System.getProperty("timestamp") + "\"");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
-	@After
-	public void tearDown() {
-		BrowserDriver.getDriver().quit();
-	}
+        // set the driver based on property above
+        BrowserDriver.setDriver(new ChromeDriver());
+        BrowserDriver.getDriver().get(webAppUrl);
+    }
 
-	@Given("I am in the twitter home page")
-	public void i_am_in_the_twitter_login_or_signup_page() {
-		hPage = new HomePage(BrowserDriver.getDriver());
-		hPage.goToLoginPage();
-	}
+    @After
+    public void tearDown() {
+        BrowserDriver.getDriver().quit();
+    }
 
-	@Given("^login as \"([^\"]*)\" with password \"([^\"]*)\"$")
-	public void loginAsGiven(String uName, String pWord) {
-		loginP = new LoginPage(BrowserDriver.getDriver());
-		loginP.loginWithCredentials(uName, pWord);
-		BrowserDriver.getDriver().findElement(By.xpath("(//button[contains(@type,'submit')])[1]")).click();
-	}
+    @Given("I am in the twitter home page")
+    public void i_am_on_Twitter_website() {
+        hPage = new HomePage(BrowserDriver.getDriver());
+        hPage.goToLoginPage();
+    }
 
-	@Then("^I should land on my personal landing page \"([^\"]*)\"$")
-	public void verifyUserLandingPage(String fullName) {
-		twitterHome = new TwitterUserHomePage(BrowserDriver.getDriver());
-		Assert.assertEquals(twitterHome.verifyDashboardProfileName(fullName), true,
-				"The fullname: \"" + fullName + "\" was not found in the dashboard profile.");
-		scenario.write("This should go to the report"); // sample line to print in cucumber html report
-	}
+    @Given("^login as \"([^\"]*)\" with password \"([^\"]*)\"$")
+    public void loginAsGiven(String uName, String pWord) {
+        loginP = new LoginPage(BrowserDriver.getDriver());
+        loginP.loginWithCredentials(uName, pWord);
+        //BrowserDriver.getDriver().findElement(By.xpath("(//button[contains(@type,'submit')])[1]")).click();
+    }
+
+    @Then("^I should land on my personal landing page \"([^\"]*)\"$")
+    public void verifyUserLandingPage(String fullName) throws Exception {
+        twitterHome = new TwitterUserHomePage(BrowserDriver.getDriver());
+        Assert.assertEquals(fullName, twitterHome.getDashboardProfileName(), "Profile name doesn't match");
+    }
 }
