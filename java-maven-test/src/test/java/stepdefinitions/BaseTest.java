5dfffb3d549b6a89054a2c0dfc63ab9cebb1610a
Riju Vijayan <riju.vijayan@qualit.co.nz>
2020-08-29 09:25:08 +1200

    final codebase for EPP 2020

--- java-maven-test/src/test/java/stepdefinitions/BaseTest.java
+++ java-maven-test/src/test/java/stepdefinitions/BaseTest.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utilities.BrowserDriver;
 
 public class BaseTest {
@@ -10,7 +11,10 @@ public class BaseTest {
     @Before
     public void setupBrowser() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        BrowserDriver.setDriver(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.setExperimentalOption("useAutomationExtension", false);
+        BrowserDriver.setDriver(new ChromeDriver(options));
     }
 
 
